abd6b0649a3ac0f4daefd5cc86eaffe231ca0f8b
davzoku <teng.kokwai93@gmail.com>
2018-01-12 21:38:13 +0800

    solve leap.js

---
+++ javascript/leap/leap.spec.js
@@ -0,0 +1,46 @@
+var Year = require('./leap');
+
+describe('Leap year', function () {
+  it('is not very common; 2015', function () {
+    var year = new Year(2015);
+    expect(year.isLeap()).toBe(false);
+  });
+
+  it('is introduced every 4 years to adjust about a day; 2016', function () {
+    var year = new Year(2016);
+    expect(year.isLeap()).toBe(true);
+  });
+
+  it('is skipped every 100 years to remove an extra day; 1900', function () {
+    var year = new Year(1900);
+    expect(year.isLeap()).toBe(false);
+  });
+
+  it('is reintroduced every 400 years to adjust another day; 2000', function () {
+    var year = new Year(2000);
+    expect(year.isLeap()).toBe(true);
+  });
+
+  // Feel free to enable the following tests to check some more examples
+  describe('Additional example of a leap year that; 1978', function () {
+    it('is not a leap year', function () {
+      var year = new Year(1978);
+      expect(year.isLeap()).toBe(false);
+    });
+
+    it('is a common leap year; 1992', function () {
+      var year = new Year(1992);
+      expect(year.isLeap()).toBe(true);
+    });
+
+    it('is skipped every 100 years; 2100', function () {
+      var year = new Year(2100);
+      expect(year.isLeap()).toBe(false);
+    });
+
+    it('is reintroduced every 400 years; 2400', function () {
+      var year = new Year(2400);
+      expect(year.isLeap()).toBe(true);
+    });
+  });
+});
