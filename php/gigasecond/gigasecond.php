6e966b62152ca6dd90808d84b19fdc8a7b592618
davzoku <teng.kokwai93@gmail.com>
2018-01-22 17:21:12 +0800

    refactor gigasecond.php

--- php/gigasecond/gigasecond.php
+++ php/gigasecond/gigasecond.php
@@ -1,8 +1,8 @@
 <?php
-/*     PT = Time Period */
-const GIGASECOND = 'PT1000000000S';
 
+const GIGASECOND = 10**9;
+/*     PT = Time Period; S = Seconds */
 function from($date){
-	return (clone $date) -> add(new DateInterval(GIGASECOND));
+	return (clone $date) -> add(new DateInterval('PT'.GIGASECOND.'S'));
 }
 ?>
