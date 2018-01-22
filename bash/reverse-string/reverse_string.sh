fbf7e03a34c7a55b6720d5b5382cd153e1c154b1
davzoku <teng.kokwai93@gmail.com>
2018-01-22 21:40:26 +0800

    refactor reverse_string.sh

--- bash/reverse-string/reverse_string.sh
+++ bash/reverse-string/reverse_string.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# $* is the list of the positional parameters, but with word splitting.
 INPUT=$*
-
-reverse() {
-# use rev
-  echo "${INPUT}" | rev
-}
-
-reverse
+echo "$INPUT" | rev
